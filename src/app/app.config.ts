06843ecfcf219e4e3580b15c071298429baf1761
FFreitas <francisco.freitas.ff@gmail.com>
2024-06-20 18:19:55 +0100

    auth interceptor created

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,12 +3,14 @@ import {provideRouter} from '@angular/router';
 
 import {routes} from './app.routes';
 import {provideHttpClient, withInterceptors} from "@angular/common/http";
-import {authenticationInterceptor} from "./auth/interceptor/authentication.interceptor";
+import {authenticationInterceptor} from "./interceptor/authentication.interceptor";
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({eventCoalescing: true}),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([authenticationInterceptor]))
+    provideHttpClient(withInterceptors([authenticationInterceptor])),
+    provideAnimationsAsync()
   ]
 };
