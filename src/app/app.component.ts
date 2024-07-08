2746e7ed9e1545f78d5811c54e14ec47156e4149
FFreitas <francisco.freitas.ff@gmail.com>
2024-07-08 17:20:09 +0100

    set frontend pipeline

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,20 @@ import {NgOptimizedImage} from "@angular/common";
 import {MatIcon} from "@angular/material/icon";
 import {MatIconButton} from "@angular/material/button";
 import {MatTooltip} from "@angular/material/tooltip";
+import {ApiModule} from "./api/api.module";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgOptimizedImage, MatIcon, MatIconButton, RouterLink, MatTooltip],
+  imports: [
+    RouterOutlet,
+    NgOptimizedImage,
+    MatIcon,
+    MatIconButton,
+    RouterLink,
+    MatTooltip
+   // ApiModule.forRoot({rootUrl: 'hhh'})
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
